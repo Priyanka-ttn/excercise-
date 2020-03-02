e04c1f42283554b83e91ad6398020adbdb3cf455
Priyanka-ttn <priyanka.singh@tothenew.com>
2020-03-02 10:19:20 +0530

    collection

--- Collection-exercise4/datastucture/SpecialStack.java
+++ Collection-exercise4/datastucture/SpecialStack.java
@@ -4,86 +4,97 @@ import java.util.*;
 
 class NewStack
 {
-    Stack<Integer> s;
-    int minElement;
+    List<Integer> list = null;
+    private int stackSize;
+    private int min = Integer.MAX_VALUE;
+    private int top = -1;
 
-
-    NewStack() {
-        s = new Stack<Integer>();
-    }
-
-    void getMin()
-    {
-
-        if (s.isEmpty())
-            System.out.println("Stack is empty");
-
-        else
-            System.out.println("Minimum Element in the stack is: " + minElement);
-    }
-
-
-    // Removes the top element
-    void pop()
-    {
-        if (s.isEmpty())
-        {
-            System.out.println("Stack is empty");
-            return;
+    NewStack(int stackSize) {
+        this.stackSize = stackSize;
+        try{
+            if (stackSize < 1) {
+                throw new Exception("Stack size cannot be less than 1");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        System.out.print("Top Most Element Removed: ");
-        Integer t = s.pop();
-
-        if (t < minElement)
-        {
-            System.out.println(minElement);
-            minElement = 2*minElement - t;
-        }
-
-        else
-            System.out.println(t);
+        list = new ArrayList<>(stackSize);
     }
 
-    // Insert new number
-    void push(Integer x)
-    {
-        if (s.isEmpty())
-        {
-            minElement = x;
-            s.push(x);
-            System.out.println("Number Inserted: " + x);
-            return;
+    void push(int ele){
+        try{
+            if (!isFull()) {
+                top++;
+                list.add(ele);
+                min = Math.min(ele, min);
+            } else {
+                throw new Exception("Stack is full cannot add more elements");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        // If new number is less than original minElement
-        if (x < minElement)
-        {
-            s.push(2*x - minElement);
-            minElement = x;
-        }
-
-        else
-            s.push(x);
-
-        System.out.println("Number Inserted: " + x);
     }
-};
 
-class Main
-{
-    public static void main(String[] args)
-    {
-        NewStack s = new NewStack();
-        s.push(3);
-        s.push(5);
-        s.getMin();
-        s.push(2);
-        s.push(1);
-        s.getMin();
-        s.pop();
-        s.getMin();
-        s.pop();
+    boolean isFull() {
+        return top == stackSize-1;
+    }
 
+    boolean isEmpty(){
+        return top == -1;
+    }
+
+    int getMin(){
+        return min;
+    }
+
+    Integer pop(){
+        try{
+            if (!isEmpty()) {
+                int popped = list.remove(top);
+                if(popped==min){
+                    changeMin();
+                }
+                top--;
+                return popped;
+            } else {
+                throw new Exception("Stack is empty cannot delete more elements");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void changeMin(){
+        min = Integer.MAX_VALUE;
+        for (Integer ele : list) {
+            min = Math.min(min,ele);
+        }
+    }
+    @Override
+    public String toString() {
+        return list.toString();
     }
 }
+
+class Stack {
+    public static void main(String[] args) {
+       NewStack stack = new NewStack(10);
+        stack.push(100);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        stack.push(60);
+        stack.push(70);
+        stack.push(80);
+        stack.push(90);
+        stack.push(10);
+
+        System.out.println(stack);
+        System.out.println("Min: " + stack.getMin());
+        System.out.println("Pop :" + stack.pop());
+        System.out.println("Current Stack :" + stack);
+        System.out.println("Min :" + stack.getMin());
+    }}
